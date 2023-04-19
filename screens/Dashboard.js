e63a3b7d5553ad56d2482051b684a87700aed8c8
Nanfahloic <yayzoy@gmail.com>
2023-04-19 10:50:30 +0100

    Created index-1

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,21 +1,97 @@
-import React from 'react';
+/** @format */
+
+import React, { useRef, useState } from "react";
 import {
-    View,
-    Text,
-    TouchableOpacity
-} from 'react-native';
+  View,
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  Animated,
+  ScrollView,
+  Platform,
+  Image,
+} from "react-native";
+import { COLORS, FONTS, SIZES, dummyData, icons, images } from "../constants";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+import { useState } from "react";
+
+const COUNTRUES_ITEM_SIZE = SIZES.width / 3;
 
 const Dashboard = ({ navigation }) => {
+  const countryScrollX = useRef(new Animated.Value(0)).current;
+  const [countries, setCountrie] = useState([
+    { id: -1 },
+    dummyData.countries,
+    { id: -2 },
+  ]);
+  function renderHeader() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Dashboard</Text>
-            <TouchableOpacity
-                onPress={() => navigation.navigate("Place")}
-            >
-                <Text>Navigate to Place</Text>
-            </TouchableOpacity>
+      <View
+        style={{
+          flexDirection: "row",
+          paddingHorizontal: SIZES.padding,
+          paddingVertical: SIZES.base,
+          alignItems: "center",
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            width: 45,
+            height: 45,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onPress={() => console.log("side drawer")}
+        >
+          <Image
+            source={icons.side_drawer}
+            resizeMode="contain"
+            style={{
+              width: 25,
+              height: 25,
+              tintColor: COLORS.white,
+            }}
+          />
+        </TouchableOpacity>
+
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: COLORS.white }}>Douala</Text>
         </View>
-    )
-}
+        <TouchableOpacity onPress={() => console.log("profile")}>
+          <Image
+            source={images.profile_pic}
+            resizeMode="contain"
+            style={{
+              width: 45,
+              height: 45,
+              borderRadius: 30,
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  function renderCountries() {}
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.black }}>
+      {renderHeader()}
+      <ScrollView
+        contentContainerStyle={{
+          paddingBottom: Platform.OS === "ios" ? 40 : 0,
+        }}
+      >
+        <View style={{ height: 700 }}>{renderCountries()}</View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Dashboard;
